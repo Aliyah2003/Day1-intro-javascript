e1f765d01b916e4cd8e78b52c9613ca7eb9b71f8
Aliyah2003 <aliyahibrahim3@gmail.com>
2024-09-02 15:57:07 +0100

    add objectDestructuring

---
+++ objectDestructuring.js
@@ -0,0 +1,10 @@
+const student={
+    name: "sammy",
+    age: 16,
+    gender: "Male"
+}
+
+function printStudent(name, age, gender, course){
+    console.log(`the student name is ${name},age is ${age}, gender is ${gender} and course is ${course}`)
+}
+printStudent(student, "computing")
