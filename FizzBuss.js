0c6dcb6b7ef813619289b98c7d9dc634386cd250
Aliyah2003 <aliyahibrahim3@gmail.com>
2024-09-02 18:07:30 +0100

    first commit

--- FizzBuss.js
+++ FizzBuss.js
@@ -11,6 +11,29 @@
         else if (i%5==0){
             console.log("Buzz")
         }
-        else console.log(i)
+        else {
+            console.log(i)
     }
+}
+function fizzBuzzGeneratorArray(element){
+    if (i%3==0 && i%5==0){
+        fizzBuzzList.push("fizzBuss")
+        }
+        else if (i%3==0){
+            fizzBuzzList.push("fizz")
+        }
+        else if (i%5==0){
+            fizzBuzzList.push("Buzz")
+        }
+        else {
+            fizzBuzzList.push(i)
+}
+}
+function fizzBuzzGeneratorArray(elements{
+
+}
+
+module.exports={
+    fizzBuzzGenerator,
+    fizzBuzzGeneratorArray
 }
---
+++ another.js
@@ -0,0 +1,4 @@
+const {fizzBuzzGenerator, fizzBuzzGeneratorArray}=require("./fizzBuzz")
+
+list=fizzBuzzGeneratorArray(15)
+console.log(list)
